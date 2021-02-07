446ad244751ac581d180d83b033815dde8896f34
andras@newzoo.com <andras@newzoo.com>
2021-02-07 10:50:31 +0100

    added MeetupFilter component

--- src/components/MeetupGrid/MeetupGrid.svelte
+++ src/components/MeetupGrid/MeetupGrid.svelte
@@ -1,19 +1,36 @@
 <script lang="ts">
     import MeetupItem from '../MeetupItem/MeetupItem.svelte'
+    import Button from '../Button/Button.svelte'
+    import MeetupFilter from '../MeetupFilter/MeetupFilter.svelte'
 
     import type { IMeetup } from '../../types/meetups'
 
     export let meetups: IMeetup[]
+
+    let favsOnly = false
+    $: filteredMeetups = favsOnly
+        ? meetups.filter((m) => m.isFavorite)
+        : meetups
+
+    function setFilter(event: CustomEvent<number>) {
+        favsOnly = !!event.detail
+    }
 </script>
 
-<section>
-    {#each meetups as meetup}
+<section id="meetup-controls">
+    <MeetupFilter on:select={setFilter} />
+</section>
+<section id="meetups">
+    {#each filteredMeetups as meetup}
         <MeetupItem {meetup} on:togglefavorite on:showDetails on:edit />
     {/each}
 </section>
 
 <style>
-    section {
+    #meetup-controls {
+        margin: 1rem;
+    }
+    #meetups {
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
@@ -21,7 +38,7 @@
     }
 
     @media (min-width: 768px) {
-        section {
+        #meetups {
             grid-template-columns: repeat(2, 1fr);
         }
     }
